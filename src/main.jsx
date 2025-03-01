5752a98d30de786c724b27e4c779f893ca2ff07c
Tamizh-Mani <manikandanlogu24@gmail.com>
2025-03-01 23:20:47 +0530

    Added dist folder for GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../node_modules/slick-carousel/slick/slick.css";
 import "../node_modules/slick-carousel/slick/slick-theme.css";
 
-createRoot(document.getElementById('root')).render(
-  <App />
-);
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+<App />
+</React.StrictMode>
+)
