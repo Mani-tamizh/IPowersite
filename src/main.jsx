5dcfb05dc27022e74c6c93a81033a11cb9699f49
Mani-tamizh <152505407+Mani-tamizh@users.noreply.github.com>
2025-03-01 21:47:43 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "../node_modules/slick-carousel/slick/slick.css";
-import "../node_modules/slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 createRoot(document.getElementById('root')).render(
   <App />
