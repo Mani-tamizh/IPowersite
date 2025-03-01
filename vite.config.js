25a14aad2e868eb798becb4af05e585f4f971ae5
Mani-tamizh <152505407+Mani-tamizh@users.noreply.github.com>
2025-03-01 21:55:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/IpowerCom/" // ✅ Make sure it starts with a "/"
+  base: "/IpowerCom" // ✅ Make sure it starts with a "/"
 });
