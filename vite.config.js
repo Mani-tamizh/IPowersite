11115f02ae12a1711adca6d4238fcce496deee86
Tamizh-Mani <manikandanlogu24@gmail.com>
2025-04-11 20:25:31 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base:"IPowersite",
+  base: "/IPowersite/", // 👈 correct path for GitHub Pages
 });
-
